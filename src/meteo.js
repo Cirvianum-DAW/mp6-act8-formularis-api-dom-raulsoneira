1db1597867d6af2af0948236aa9eeade9657b747
Raul Soneira <raul.soneira@cirvianum.cat>
2024-04-12 13:56:24 +0200

    acabat

--- src/meteo.js
+++ src/meteo.js
@@ -16,7 +16,7 @@ document
     try {
       const apiKey = '3bd1ae79e6a04d44873144237241004';
 
-      const currentWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${ciutat}&lang=ko `;
+      const currentWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${ciutat}&lang=ko`;
       const forecastWeatherUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${ciutat}&lang=ko&days=2`;
 
       const [currentResponse, forecastResponse] = await Promise.all([
@@ -39,7 +39,7 @@ document
           regio: currentData.location.region,
           weatherIcon: currentData.current.condition.icon,
           temperatura: `${currentData.current.temp_c}°C`,
-          weatherDescription: currentData.condition.text,
+          weatherDescription: currentData.current.condition.text,
         },
 
         forecast: {
@@ -48,7 +48,7 @@ document
           regio: forecastData.location.region,
           weatherIcon: forecastData.forecast.forecastday[1].day.condition.icon,
           temperatura: `${forecastData.forecast.forecastday[1].day.avgtemp_c}°C`,
-          weatherDescription: forecastData.forecast.forecastday[1].day.text,
+          weatherDescription: forecastData.forecast.forecastday[1].day.condition.text,
         },
       };
 
@@ -57,9 +57,24 @@ document
       displayWeatherResults(weatherData);
     } catch (error) {
         console.error("Error en la petició a l'api", error);
+        alert("Hi ha hagut un error en la petició a l'API. Si us plau, intenta-ho de nou més tard.");
+
     }
   });
 
   function displayWeatherResults(weatherData) {
-    
-  }
+      // Mostrar la información actual
+      document.getElementById('location-text').textContent = `${weatherData.current.ciutat}, ${weatherData.current.pais}, ${weatherData.current.regio}`;
+      document.getElementById('temperature').textContent = `Temperatura actual: ${weatherData.current.temperatura}`;
+      document.getElementById('weather-description').textContent = `Descripció del temps actual: ${weatherData.current.weatherDescription}`;
+      document.getElementById('weather-icon').src = weatherData.current.weatherIcon;
+  
+      // Mostrar la información de la previsión para el día siguiente
+      document.getElementById('tomorrow-temperature').textContent = `Temperatura prevista per al dia següent: ${weatherData.forecast.temperatura}`;
+      document.getElementById('tomorrow-weather-description').textContent = `Descripció del temps previst per al dia següent: ${weatherData.forecast.weatherDescription}`;
+      document.getElementById('tomorrow-weather-icon').src = weatherData.forecast.weatherIcon;
+  
+      // Mostrar los resultados
+      document.getElementById('results').classList.remove('hidden');
+}
+  
